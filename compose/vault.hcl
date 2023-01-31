c812131d3893af801e7e4d771a72ee96daa20d9c
Mark West <mark.west@hashicorp.com>
2023-01-31 08:37:19 -0800

    WIP

--- compose/vault.hcl
+++ compose/vault.hcl
@@ -18,7 +18,7 @@ listener "tcp" {
   tls_disable = true
 }
 
-api_addr = "http://0.0.0.0:8200"
+api_addr = "http://127.0.0.1:8200"
 cluster_addr = "http://127.0.0.1:8201"
 
 license_path = "/opt/vault/lic/vault.hclic"
--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     vault = {
       source = "hashicorp/vault"
-      version = "3.10.0"
+      version = "3.12.0"
     }
   }
 }
@@ -11,7 +11,6 @@ provider "vault" {
   # Configuration options
   address = "http://127.0.0.1:8200"
   skip_tls_verify = true
-  token = "hvs.nlqKigaigbIrq9ewwJE4X4DY"
 }
 
 data "vault_policy_document" "kmip" {
@@ -32,9 +31,11 @@ data "vault_policy_document" "kmip" {
   }
 }
 
-resource "vault_policy" "kmip" {
-  name   = "kmip_policy"
-  policy = data.vault_policy_document.kmip.hcl
+locals {
+  encryption_types = toset([
+    "FLE",
+    "QUERYABLE",
+  ])
 }
 
 resource "vault_kmip_secret_backend" "default" {
@@ -42,21 +43,57 @@ resource "vault_kmip_secret_backend" "default" {
   description = "Vault KMIP backend"
 }
 
-resource "vault_kmip_secret_scope" "dev" {
+resource "vault_kmip_secret_scope" "mongodb" {
   path  = vault_kmip_secret_backend.default.path
-  scope = "dev"
+  scope = "mongodb"
   force = true
 }
 
-resource "vault_kmip_secret_role" "admin" {
-  path                     = vault_kmip_secret_scope.dev.path
-  scope                    = vault_kmip_secret_scope.dev.scope
+resource "vault_kmip_secret_scope" "vmware" {
+  path  = vault_kmip_secret_backend.default.path
+  scope = "vmware"
+  force = true
+}
+
+resource "vault_kmip_secret_scope" "my-service" {
+  path  = vault_kmip_secret_backend.default.path
+  scope = "my-service"
+  force = true
+}
+
+resource "vault_kmip_secret_role" "mongodb_admin" {
+  path                     = vault_kmip_secret_scope.mongodb.path
+  scope                    = vault_kmip_secret_scope.mongodb.scope
   role                     = "admin"
-  tls_client_key_type      = "ec"
-  tls_client_key_bits      = 256
+  tls_client_key_type      = "rsa"
+  tls_client_key_bits      = 2048
   operation_activate       = true
   operation_get            = true
   operation_get_attributes = true
   operation_create         = true
   operation_destroy        = true
+}
+
+
+resource "vault_kmip_secret_role" "vmware_admin" {
+  path                     = vault_kmip_secret_scope.vmware.path
+  scope                    = vault_kmip_secret_scope.vmware.scope
+  role                     = "admin"
+  tls_client_key_type      = "rsa"
+  tls_client_key_bits      = 2048
+  operation_activate       = true
+  operation_get            = true
+  operation_get_attributes = true
+  operation_create         = true
+  operation_destroy        = true
+}
+
+
+resource "vault_kmip_secret_role" "my-service" {
+  path                     = vault_kmip_secret_scope.my-service.path
+  scope                    = vault_kmip_secret_scope.my-service.scope
+  role                     = "admin"
+  tls_client_key_type      = "rsa"
+  tls_client_key_bits      = 2048
+  operation_all            = true
 }
